4ae1fdebd634937c5c5e87852f5d67a07d951c1c
liqiu <liqiu@czcg.com.cn>
2016-11-10 10:47:16 +0800

    liqiu：解决冲突

--- start/fc.js
+++ start/fc.js
@@ -6,20 +6,17 @@ glob.use('TYPE', 'HTTP');
 glob.use('ROOT', __dirname);
 glob.use('REQUEST', {
   'GET': {
-<<<<<<< HEAD
     '/fanacing': path + '/fanacing.json',
     '/orderfinance': path + '/orderfinance.json',
     '/orderFinanceInit': path + '/orderFinanceInit.json',
     '/login': path + '/login.json',
     '/financingInfo': path + '/financingInfo.json',
     '/financeList': path + '/financeList.json',
-    '/repayList': path + '/repayList.json'
-=======
+    '/repayList': path + '/repayList.json',
     '/gt/indexInit': path + '/fanacing.json',
     '/bu/orders': path + '/orderfinance.json',
     '/bu/orderFinanceInit': path + '/orderFinanceInit.json',
     '/login': path + '/login.json'
->>>>>>> cc00dad715d7a19bfdbffcb071b2554a7c2fd84e
   },
   'POST': {
   }

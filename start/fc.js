cc00dad715d7a19bfdbffcb071b2554a7c2fd84e
Jim Yuan <aquarior@gmail.com>
2016-11-10 09:58:17 +0800

    融资数据修改

--- start/fc.js
+++ start/fc.js
@@ -6,9 +6,9 @@ glob.use('TYPE', 'HTTP');
 glob.use('ROOT', __dirname);
 glob.use('REQUEST', {
   'GET': {
-    '/fanacing': path + '/fanacing.json',
-    '/orderfinance': path + '/orderfinance.json',
-    '/orderFinanceInit': path + '/orderFinanceInit.json',
+    '/gt/indexInit': path + '/fanacing.json',
+    '/bu/orders': path + '/orderfinance.json',
+    '/bu/orderFinanceInit': path + '/orderFinanceInit.json',
     '/login': path + '/login.json'
   },
   'POST': {

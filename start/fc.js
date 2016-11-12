5b72ed8dcea1149e4696043a21b34139c2e84c2d
zhaohaiming <1072950174@qq.com>
2016-11-12 14:15:21 +0800

    zhao

--- start/fc.js
+++ start/fc.js
@@ -9,9 +9,15 @@ glob.use('REQUEST', {
     '/fanacing': path + '/fanacing.json',
     '/orderfinance': path + '/orderfinance.json',
     '/orderFinanceInit': path + '/orderFinanceInit.json',
-    '/login': path + '/login.json'
+    '/login': path + '/login.json',
+    '/bu/creditApplyInit': path + '/creditApplyInit.json',
+    '/ta/creditManagerInit':path+'/creditManagerInit.json',
+    '/ta/creditAccoDetails':path+'/creditAccoDetails.json',
+    '/ta/rece/creditManagerInit':path+'/fp-creditManagerInit.json',
+    '/bu/creditApply':path+'/creditApply.json'
   },
   'POST': {
+    // '/bu/creditApply':path+'/creditApply.json'
   }
 });
 glob.initialize();

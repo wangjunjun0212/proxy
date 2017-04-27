f6a7689a19f07652cf0e1175cbef0afa8d33acbe
Jim Yuan <aquarior@gmail.com>
2017-04-27 17:27:27 +0800

    ws data updata

--- start/ws.js
+++ start/ws.js
@@ -6,11 +6,13 @@ glob.use('TYPE', 'HTTP');
 glob.use('ROOT', __dirname);
 glob.use('REQUEST', {
   'GET': {
-    "/trade/other": path +"/91steel.json",
-    "/trade/getToday": path +"/91steel-update.json",
-    "/finance/other": path +"/ebaoli.json",
-    "/finance/progress": path +"/ebaoli-progress.json",
-    "/finance/getToday": path +"/ebaoli-update.json"
+    "/trade/other": path + "/91steel.json",
+    "/trade/getToday": path + "/91steel-update.json",
+    "/finance/other": path + "/ebaoli.json",
+    "/finance/progress": path + "/ebaoli-progress.json",
+    "/finance/getToday": path + "/ebaoli-update.json",
+    "/dzy/getDZYScreenRightData": path + "/9156.json",
+    "/dzy/getDZYScreenLeftData": path + "/9156-order.json"
   },
   'POST': {
 
